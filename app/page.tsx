41ddbbf5ff666ffad969b6e62881e8e4f634e2d9
carolinenorgaard <caroline.norgaard.elmer@gmail.com>
2025-11-18 16:46:50 +0100

    Added LoginForm and PrimaryCard components to the Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
-import PrimaryButton from "./components/PrimaryButton";
-
+import LoginForm from "./components/LoginForm"
+import PrimaryButton from "./components/PrimaryButton"
+import PrimaryCard from "./components/PrimaryCard"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-white">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans bg-white">
       <PrimaryButton />
+      <div className="mt-8 max-w-sm mx-auto bg-white p-4 rounded-xl shadow">
+        <PrimaryCard />
       </div>
-
-  );
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
