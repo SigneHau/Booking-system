9579cb81fad426807f71896536315f9c45fd0e11
Signe Kjær Brandt Hau <signehau@gmail.com>
2025-11-18 15:15:45 +0100

    opdateret baggrund farve

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import PrimaryButton from "./components/PrimaryButton";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-white">
       <PrimaryButton />
       </div>
 
