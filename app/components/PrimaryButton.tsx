3195d4abef5802b11d8f6bbb2836ff57baf10261
Signe Kjær Brandt Hau <signehau@gmail.com>
2025-11-18 15:12:34 +0100

    primarybutton conponents

---
+++ app/components/PrimaryButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@mantine/core'
+
+const PrimaryButton = () => {
+  return (
+    
+    <Button variant="filled" color="teal" radius="md">Button</Button>
+   
+  )
+}
+
+export default PrimaryButton
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import PrimaryButton from "./components/PrimaryButton";
 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hej med dig - nu er vi igang </div>
+      <PrimaryButton />
+      </div>
+
   );
 }
