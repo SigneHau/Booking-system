dc7f131117fe7ca234a48a24297afdc644aff6d1
Signe Kjær Brandt Hau <signehau@gmail.com>
2025-11-18 09:12:28 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Hej med dig </div>
+      Hej med dig - nu er vi igang </div>
   );
 }
