c6f4030675ee6206c5136ad7ad0a3702929ab1cf
carolinenorgaard <caroline.norgaard.elmer@gmail.com>
2025-12-03 21:39:19 +0100

    Opdateret RoleBadge-komponenten til at bruge danske strenglitteraler for roller. Forbedret kodekonsistens ved at fjerne unødvendige semikolon og justere return-sætninger.

--- app/components/RoleBadge.tsx
+++ app/components/RoleBadge.tsx
@@ -1,31 +1,31 @@
-"use client";
-import { useUser } from "@/hooks/useUser";
+"use client"
+import { useUser } from "@/hooks/useUser"
 
 const getRoleLabel = (role?: string) => {
   switch (role) {
     case "Student":
-      return "student";
+      return "elev"
     case "Teacher":
-      return "lærer";
+      return "lærer"
     default:
-      return "ukendt";
+      return "ukendt"
   }
-};
+}
 
 export default function RoleBadge() {
-  const { user, loading } = useUser();
+  const { user, loading } = useUser()
 
   if (loading) {
     return (
       <span className="inline-block bg-gray-200 px-3 py-1 text-xs rounded-md mt-2 w-[150px] ">
         Henter brugerdata...
       </span>
-    );
+    )
   }
 
   return (
     <p className="inline-block bg-green-200 px-3 py-1 text-center text-xs font-light rounded-md mt-2 w-fit">
       Logget ind som {getRoleLabel(user?.role)}
     </p>
-  );
+  )
 }
